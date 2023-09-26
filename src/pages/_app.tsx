221d5e77286001e4a7068d7ebdf6fd20b3538d6f
PedroAugusCoelho <peaugusto.coelho@gmail.com>
2023-09-26 11:56:37 -0300

    add stripe and getStaticProps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
-import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
+import { globalStyles } from '@/styles/global'
+
+import LogoImg from '../assets/Logo.svg'
+import { Container, Header } from '@/styles/pages/app'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={LogoImg} alt='ignite-shop' />
+      </Header>
+      
+      <Component {...pageProps} />
+    </Container>
+  )
 }
